84a844c6dd18cf92ebb124c8560ea276ac7cb58a
vivek <vivek23051998@gmail.com>
2024-09-01 22:07:41 +0530

    order-service added

---
+++ dev/Order-Service/src/main/java/com/programmingtechie/Order_Service/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.programmingtechie.Order_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
---
+++ dev/Order-Service/src/test/java/com/programmingtechie/Order_Service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingtechie.Order_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
