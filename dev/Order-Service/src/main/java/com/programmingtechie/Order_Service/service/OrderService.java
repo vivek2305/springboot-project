2ed9e6eda27aa86c0f828224a33146c01ebffab7
vivek <vivek23051998@gmail.com>
2024-09-01 22:37:21 +0530

    order-service with mysql connection.

---
+++ dev/Order-Service/src/main/java/com/programmingtechie/Order_Service/service/OrderService.java
@@ -0,0 +1,40 @@
+package com.programmingtechie.Order_Service.service;
+
+import com.programmingtechie.Order_Service.dto.OrderLineItemsDto;
+import com.programmingtechie.Order_Service.dto.OrderRequest;
+import com.programmingtechie.Order_Service.model.Order;
+import com.programmingtechie.Order_Service.model.OrderLineItems;
+import com.programmingtechie.Order_Service.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest){
+        Order order= new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> OrderLineItems= orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+        order.setOrderLineItemsList(OrderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems=new OrderLineItems();
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+        return orderLineItems;
+
+    }
+}
